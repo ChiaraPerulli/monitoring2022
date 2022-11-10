6abb20d33a6da45980b442e1d4f9f8425b2e3213
ChiaraPerulli <114755961+ChiaraPerulli@users.noreply.github.com>
2022-11-10 17:18:40 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -13,5 +13,15 @@ brick("p224r63_2011_masked.grd")
 # assign the brick function to a certain name
 p224r63_2011 <- brick("p224r63_2011_masked.grd") 
 
+# we can plot the single bands (each band shows the amount of reflectance in a certain wavelenght)
+plot(p224r63_2011)
+
+# we can change the colour of the bands with the function colorRampPalette
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 100 is the amount of different tones 
+
+# replot with the new colour palette
+plot(p224r63_2011, col=cl)
+
+# 
 
 
