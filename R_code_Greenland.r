d26430960b569bcbc44e727cbf01c293a1f12524
ChiaraPerulli <114755961+ChiaraPerulli@users.noreply.github.com>
2022-12-02 17:42:59 +0100

    Update R_code_Greenland.r

--- R_code_Greenland.r
+++ R_code_Greenland.r
@@ -50,16 +50,34 @@ TGr <- stack(import)    # now you have a single file with the 4 layers
 # 4. plot the data
 plot(TGr)
 
+# first ggplot
+ggplot() + geom_raster (TGr$lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) + scale_fill_viridis(option="mako", direction=-1, alpha=0.8) + ggtitle("Temperature 2000")
+# or
+p1 <- ggplot() + geom_raster (TGr[[1]], mapping=aes(x=x, y=y, fill=lst_2000)) + scale_fill_viridis(option="mako", direction=-1, alpha=0.8) + ggtitle("Temperature 2000")
 
+# second ggplot
+p2 <- ggplot() + geom_raster (TGr$lst_2005, mapping=aes(x=x, y=y, fill=lst_2005)) + scale_fill_viridis(option="mako", direction=-1, alpha=0.8) + ggtitle("Temperature 2005")
 
+# third plot
+p3 <- ggplot() + geom_raster (TGr$lst_2010, mapping=aes(x=x, y=y, fill=lst_2010)) + scale_fill_viridis(option="mako", direction=-1, alpha=0.8) + ggtitle("Temperature 2010")
 
+# fourth plot
+p4 <- ggplot() + geom_raster (TGr$lst_2015, mapping=aes(x=x, y=y, fill=lst_2015)) + scale_fill_viridis(option="mako", direction=-1, alpha=0.8) + ggtitle("Temperature 2015")
 
+library(patchwork)
+p1 + p2 + p3 + p4
 
+p1 + p4
 
+# Excercise: make the difference in temperature between 2015 and 2000
+2015 - 2000
+dift <- TGr[[4]] - TGr[[1]]
+p5 <- ggplot() + geom_raster (dift, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno", direction=-1, alpha=0.8) + ggtitle("Temperatures 2015 - 2000")
 
+p1 + p4 + p5
 
-
-
+# Exercise
+plotRGB(TGr, r=1, g=2, b=4, stretch="lin")   # the parts with higher temperature will become red for 2000, green for 2005 and blue for 2015
 
 
 
